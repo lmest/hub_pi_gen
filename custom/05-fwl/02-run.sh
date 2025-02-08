bc7e937b38cebb523b495e8affdfd9b22e047598
Marcelo Barros de Almeida <marcelo.barros@ufu.br>
2025-02-08 12:22:45 -0300

    using pip even not recommended

--- custom/05-fwl/02-run.sh
+++ custom/05-fwl/02-run.sh
@@ -1,32 +1,30 @@
 #!/bin/bash -e
 # $1 destination
 
-log "Installing Multitimer..."
-if [ -d "${ROOTFS_DIR}/tmp/multitimer-master/" ]; then
-    rm -fR "${ROOTFS_DIR}/tmp/multitimer-master/"
-fi
-cp files/multitimer-master.zip "${ROOTFS_DIR}/tmp/"
-on_chroot << EOF
-unzip /tmp/multitimer-master.zip -d /tmp
-(cd /tmp/multitimer-master/ ; python3 setup.py install)
-EOF
+log "-----------------------------------------------------"
+log "          Full Wireless custom files"
+log "-----------------------------------------------------"
+#log "Installing Multitimer..."
+#if [ -d "${ROOTFS_DIR}/tmp/multitimer-master/" ]; then
+#    rm -fR "${ROOTFS_DIR}/tmp/multitimer-master/"
+#fi
+#cp files/multitimer-master.zip "${ROOTFS_DIR}/tmp/"
+#on_chroot << EOF
+#unzip /tmp/multitimer-master.zip -d /tmp
+#(cd /tmp/multitimer-master/ ; python3 setup.py install)
+#EOF
 
-log "Installing atcom..."
-if [ -d "${ROOTFS_DIR}/tmp/atcom-master/" ]; then
-    rm -fR "${ROOTFS_DIR}/tmp/atcom-master/"
-fi
-cp files/atcom-master.zip "${ROOTFS_DIR}/tmp/"
+log "Installing pip dependencies: atcom, multitimer, pyserial"
+#if [ -d "${ROOTFS_DIR}/tmp/atcom-master/" ]; then
+#    rm -fR "${ROOTFS_DIR}/tmp/atcom-master/"
+#fi
+#cp files/atcom-master.zip "${ROOTFS_DIR}/tmp/"
+#on_chroot << EOF
+#unzip /tmp/atcom-master.zip -d /tmp
+#(cd /tmp/atcom-master/ ; python3 setup.py install)
+#EOF
 on_chroot << EOF
-unzip /tmp/atcom-master.zip -d /tmp
-(cd /tmp/atcom-master/ ; python3 setup.py install)
-EOF
-
-log "Removing swap..."
-on_chroot << EOF
-sudo apt remove -y --purge dphys-swapfile
-if [ -f /var/swap ]; then
-    sudo rm /var/swap
-fi
+pip3 install atcom multitimer pyserial --break-system-packages 
 EOF
 
 if [ ${IMAGE_TYPE} = "lte" ]; then
@@ -40,3 +38,11 @@ if [ ${IMAGE_TYPE} = "lte" ]; then
     (cd /tmp/Sixfab_PPP_Installer-master/ ; chmod +x ppp_install.sh ; ./ppp_install.sh)
 EOF
 fi
+
+log "Removing swap..."
+on_chroot << EOF
+sudo apt remove -y --purge dphys-swapfile
+if [ -f /var/swap ]; then
+    sudo rm /var/swap
+fi
+EOF
